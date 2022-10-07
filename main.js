14c74e9ef4013191324656bda1929201e95c6216
Yelena Cox <yelenacox@gmail.com>
2022-10-07 09:59:01 -0500

    first commit

---
+++ main.js
@@ -0,0 +1,39 @@
+const add = (firstNumber, secondNumber) => {
+    const sum = firstNumber + secondNumber
+    return sum
+}
+
+let resultAdd = add(5, 2)
+console.log(resultAdd)
+
+const subtract = (firstNumber, secondNumber) => {
+    const difference = firstNumber - secondNumber
+    return difference
+}
+
+let resultSubtract = subtract(3, 2)
+console.log(resultSubtract)
+
+const divide = (firstNumber, secondNumber) => {
+    const quotient = firstNumber / secondNumber
+    return quotient
+}
+
+let resultDivide = divide(3, 6)
+console.log(resultDivide)
+
+const multiply = (firstNumber, secondNumber) => {
+    const product = firstNumber * secondNumber
+    return product
+}
+
+const resultMultiply = multiply(4, 2)
+console.log(resultMultiply)
+
+const square = (number) => {
+    const squared = number * number 
+    return squared
+}
+
+let resultSquare = square(3)
+console.log(resultSquare)
